5ee762b91ea6881a4db63bf249499cbf10a0cf1a
Robert Vásquez <vasquezrober09@gmail.com>
2023-04-29 13:19:36 -0400

    finished dropdown menu for shopping cart

--- js/main.js
+++ js/main.js
@@ -6,12 +6,42 @@ const desktopMenu = document.querySelector(".desktop-menu");
 const menuHamburguer = document.querySelector(".menu");
 const menuMobile = document.querySelector(".mobile-menu");
 
+//  Variables shopping cart
+const shoppingCart =document.querySelector(".navbar-shopping-cart");
+const productDetail = document.querySelector(".product-detail");
+
 // SHOW AND HIDE MENU DESKTOP
 menuEmail.addEventListener("click", () => {
+  const isMobileShoppingCartClose = !productDetail.classList.contains('inactive-product-detail');
+
   desktopMenu.classList.toggle("inactive-menu");
+
+  if(isMobileShoppingCartClose) {
+    productDetail.classList.toggle("inactive-product-detail");
+  }  
+
 });
 
 // SHOW AND HIDE MENU MOBILE
 menuHamburguer.addEventListener("click", () => {
+  const isMobileShoppingCartClose = !productDetail.classList.contains('inactive-product-detail');
+
   menuMobile.classList.toggle("inactive-menu-mobile");
+  
+  if(isMobileShoppingCartClose) {
+    productDetail.classList.toggle("inactive-product-detail");
+  }
+});
+
+// SHOW AND HIDE PRODUCT DETAIL CART
+shoppingCart.addEventListener("click", () => {
+
+  const isMobileMenuOpen = menuMobile.classList.contains('inactive-menu-mobile');
+
+  productDetail.classList.toggle("inactive-product-detail");
+
+  if(isMobileMenuOpen){
+    menuMobile.classList.toggle("inactive-menu-mobile");
+  }
+
 });
