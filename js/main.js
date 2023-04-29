12c0ce2ba5499409e0d323435fb12ddc4d2b5c12
Robert Vásquez <vasquezrober09@gmail.com>
2023-04-29 11:57:13 -0400

    finished dropdown menu for mobile mobile

--- js/main.js
+++ js/main.js
@@ -1,7 +1,17 @@
+// Variables desktop
 const menuEmail = document.querySelector(".navbar-email");
 const desktopMenu = document.querySelector(".desktop-menu");
 
-// SHOW AND HIDE MENU
+// Variables mobile
+const menuHamburguer = document.querySelector(".menu");
+const menuMobile = document.querySelector(".mobile-menu");
+
+// SHOW AND HIDE MENU DESKTOP
 menuEmail.addEventListener("click", () => {
   desktopMenu.classList.toggle("inactive-menu");
+});
+
+// SHOW AND HIDE MENU MOBILE
+menuHamburguer.addEventListener("click", () => {
+  menuMobile.classList.toggle("inactive-menu-mobile");
 });
