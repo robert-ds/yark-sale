81f706e30c30cb8e3cd26f4211953d78597b8b39
Robert Vásquez <vasquezrober09@gmail.com>
2023-04-30 11:03:27 -0400

    Created template section with js for load dinamic data of products

--- js/main.js
+++ js/main.js
@@ -45,3 +45,55 @@ shoppingCart.addEventListener("click", () => {
   }
 
 });
+
+// Dinamic Data of products
+const productList = [];
+
+for(let i = 0; i < 6; i++) {
+  productList.push({
+    name: 'Nike',
+    price: 120,
+    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+  
+  });
+}
+
+const cardsContainer = document.querySelector('.cards-container');
+
+for(product of productList){
+  const productCard = document.createElement('div');
+  productCard.classList.add('product-card');
+
+  const img = document.createElement('img');
+  img.setAttribute('src', product.image);
+
+  const productInfo = document.createElement('div');
+  productInfo.classList.add('product-info');
+
+  const productInfoDiv = document.createElement('div');
+
+  const productPrice = document.createElement('p');
+  productPrice.innerText = '$' + product.price;
+
+  const productName = document.createElement('p');
+  productName.innerText = product.name;
+
+  productInfoDiv.appendChild(productPrice);
+  productInfoDiv.appendChild(productName);
+
+  const productInfoFigure = document.createElement('figure');
+
+  const productInfoImgCard = document.createElement('img');
+  productInfoImgCard.setAttribute('src', 'icons/bt_add_to_cart.svg');
+
+  productInfoFigure.appendChild(productInfoImgCard);
+  
+  productInfo.appendChild(productInfoDiv);
+  productInfo.appendChild(productInfoFigure);
+
+  productCard.appendChild(img);
+  productCard.appendChild(productInfo);
+
+  cardsContainer.appendChild(productCard);
+
+}
