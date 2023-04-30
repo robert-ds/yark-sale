d43062718fa4cc89c9d79960219ecd0ea69d3893
Robert Vásquez <vasquezrober09@gmail.com>
2023-04-30 12:07:10 -0400

    added product detail target to project

--- js/main.js
+++ js/main.js
@@ -8,28 +8,28 @@ const menuMobile = document.querySelector(".mobile-menu");
 
 //  Variables shopping cart
 const shoppingCart =document.querySelector(".navbar-shopping-cart");
-const productDetail = document.querySelector(".product-detail");
+const shoppingCardContainer = document.querySelector("#shopping-card-container");
 
 // SHOW AND HIDE MENU DESKTOP
 menuEmail.addEventListener("click", () => {
-  const isMobileShoppingCartClose = !productDetail.classList.contains('inactive-product-detail');
+  const isMobileShoppingCartClose = !shoppingCardContainer.classList.contains('inactive-product-detail');
 
   desktopMenu.classList.toggle("inactive-menu");
 
   if(isMobileShoppingCartClose) {
-    productDetail.classList.toggle("inactive-product-detail");
+    shoppingCardContainer.classList.toggle("inactive-product-detail");
   }  
 
 });
 
 // SHOW AND HIDE MENU MOBILE
 menuHamburguer.addEventListener("click", () => {
-  const isMobileShoppingCartClose = !productDetail.classList.contains('inactive-product-detail');
+  const isMobileShoppingCartClose = !shoppingCardContainer.classList.contains('inactive-product-detail');
 
   menuMobile.classList.toggle("inactive-menu-mobile");
   
   if(isMobileShoppingCartClose) {
-    productDetail.classList.toggle("inactive-product-detail");
+    shoppingCardContainer.classList.toggle("inactive-product-detail");
   }
 });
 
@@ -38,7 +38,7 @@ shoppingCart.addEventListener("click", () => {
 
   const isMobileMenuOpen = menuMobile.classList.contains('inactive-menu-mobile');
 
-  productDetail.classList.toggle("inactive-product-detail");
+  shoppingCardContainer.classList.toggle("inactive-product-detail");
 
   if(isMobileMenuOpen){
     menuMobile.classList.toggle("inactive-menu-mobile");
